4a57c81d5e805e18fb8d44b17956b12f553b0780
trinhhung <trinhhunglbx@gmail.com>
2022-07-22 20:30:57 +0700

    Giai_pt_bac2

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
 // tính phương trình bậc 2
 // ax2+bx+c=0
 
-function giai(form){
-    var x1,x2;
-    var x,y,z,delta;
-    x= parseInt(form.a.value);
-    y= parseInt(form.b.value);
-    z= parseInt(form.c.value);
-    delta=(y*y-4*x*z)
-    if(delta ==0){
-     alert("phuong trinh co nghiem kep");
-     x1=-y/(2*x) ;
-     x2=-y/(2*x);
-     form.x.value=eval(x1);
-     form.y.value=eval(x2);
-    }else if(delta<0){
-        alert("phuong trinh vo nghiem");
+function giai(form) {
+    var x1, x2;
+    var x, y, z, delta;
+
+    x = parseInt(form.a.value);
+    y = parseInt(form.b.value);
+    z = parseInt(form.c.value);
+
+    delta = (y * y - 4 * x  *z)
+    if (delta ==0) {
+        alert("Phương trình có nghiệm kép!");
+        x1 = -y / (2 * x);
+        x2 = -y / (2 * x);
+        form.x.value = eval(x1);
+        form.y.value = eval(x2);
+    }else if (delta < 0) {
+        alert("Phương trình vô nghiệm!");
     }else{
-        alert("phuong trinh co hai nghiem");
-        x1=(-y-Math.sqrt(delta))/(2*x);
-        x2=(-y+Math.sqrt(delta))/(2*x);
-        form.x.value=eval(x1);
-        form.y.value=eval(x2);
+        alert("Phương trình có hai nghiệm phân biệt!");
+        x1 = (-y - Math.sqrt(delta)) / (2 * x);
+        x2 = (-y + Math.sqrt(delta)) / (2 * x);
+        form.x.value = eval(x1);
+        form.y.value = eval(x2);
     }
 }
